5642da4ff44d3a3ac3140cc9ed8d7ac2c59bd532
HidenoriMatsubayashi <Hidenori.Matsubayashi@sony.com>
2021-07-19 16:12:47 +0900

    Fix plugin build

--- lib/elinux_cmake_project.dart
+++ lib/elinux_cmake_project.dart
@@ -3,6 +3,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// @dart = 2.8
+
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/project.dart';
 
@@ -50,5 +52,7 @@ class ELinuxProject extends FlutterProjectPlatform
   Directory get ephemeralDirectory =>
       managedDirectory.childDirectory('ephemeral');
 
-  Future<void> ensureReadyForPlatformSpecificTooling() async {}
+  Future<void> ensureReadyForPlatformSpecificTooling() async {
+    await refreshELinuxPluginsList(parent);
+  }
 }
